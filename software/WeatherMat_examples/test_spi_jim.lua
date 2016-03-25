919f15ad8470c3e761b1a0607d2f3f63909665be
Jim Harris <JHarris@idealaero.com>
2016-03-25 10:12:24 -0600

    More cleanup

---
+++ software/WeatherMat_examples/test_spi_jim.lua
@@ -0,0 +1,19 @@
+--
+-- lua-periphery by vsergeev
+-- https://github.com/vsergeev/lua-periphery
+-- License: MIT
+--
+
+--require('test')
+local periphery = require('periphery')
+local SPI = periphery.SPI
+
+local spi = SPI("/dev/spidev0.0", 0, 5e4)
+
+local data_out = {0x01, 0x00, 0x00}
+local data_in = spi:transfer(data_out)
+
+print(string.format("shifted out {0x%02x, 0x%02x, 0x%02x}", unpack(data_out)))
+print(string.format("shifted in  {0x%02x, 0x%02x, 0x%02x}", unpack(data_in)))
+
+spi:close()
